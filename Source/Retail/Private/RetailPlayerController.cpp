dbb5e5adb49df1f2b3c6812735573524f5af34b0
Tom <tomshinton@googmail.com>
2020-09-27 23:59:16 +0100

    Added basic WASD movement with clamped zooming
    TODO: Add MiddleMouse camera movement to control pitch/rotation

--- Source/Retail/Private/RetailPlayerController.cpp
+++ Source/Retail/Private/RetailPlayerController.cpp
@@ -2,3 +2,25 @@
 
 #include "Retail/Public/RetailPlayerController.h"
 
+ARetailPlayerController::ARetailPlayerController(const FObjectInitializer& ObjectInitializer)
+	: Super(ObjectInitializer)
+{
+	bShowMouseCursor = true;
+	bEnableClickEvents = true;
+	bEnableMouseOverEvents = true;
+}
+
+void ARetailPlayerController::BeginPlay()
+{
+	Super::BeginPlay();
+
+	FInputModeGameAndUI InputMode;
+
+	InputMode
+		.SetLockMouseToViewportBehavior(EMouseLockMode::LockAlways)
+		.SetHideCursorDuringCapture(false)
+		.SetWidgetToFocus(nullptr)
+		.SetMouseCaptureMode(EMouseCaptureMode::CapturePermanently_IncludingInitialMouseDown);
+
+	SetInputMode(InputMode);
+}
